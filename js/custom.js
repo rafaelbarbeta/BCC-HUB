d2cf7e74a1c72b63d513d3c9ab7038c269f675e3
Rafael Barbeta <rafa.barbeta@gmail.com>
2023-09-09 14:44:40 -0300

    Melhora responsividade. Adiciona site de adivinhação de música

--- js/custom.js
+++ js/custom.js
@@ -20,9 +20,45 @@ $(function () {
 			loop: true, // Repetir o carrossel
 			margin: 0, // Margem entre os itens
 			autoplay: true, // Autoplay
-			autoplayTimeout: 1500, // Tempo entre os slides em milissegundos
+			autoplayTimeout: 4500, // Tempo entre os slides em milissegundos
 			autoplayHoverPause: true // Pausar o autoplay quando o mouse estiver sobre o carrossel
 		});
 	});
 
+});
+
+/* change sites div a img images for mobile */
+var last_window_size = $(window).width();
+$(document).ready(function() {
+    $(window).trigger('resize');
+	if ($(window).width() < 575) {
+		$('.sites div a img').each(function(){
+			var x = $(this).attr('src').replace('desktop', 'mobile');
+			$(this).attr('src', x);
+		});
+	}
+	else {
+		$('.sites div a img').each(function(){
+			var x = $(this).attr('src').replace('mobile', 'desktop');
+			$(this).attr('src', x);
+		});
+	}
+});
+
+$(window).resize(function(){
+	if (Math.abs(last_window_size - $(window).width()) > 50) {
+		if ($(window).width() < 575) {
+			$('.sites div a img').each(function(){
+				var x = $(this).attr('src').replace('desktop', 'mobile');
+				$(this).attr('src', x);
+			});
+		}
+		else {
+			$('.sites div a img').each(function(){
+				var x = $(this).attr('src').replace('mobile', 'desktop');
+				$(this).attr('src', x);
+			});
+		}
+		last_window_size = $(window).width();
+	}
 });
